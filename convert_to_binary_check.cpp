166d04dc3a861a2eee01341df7e1f7576349b144
AlissonRM19 <alisson.r06@estudiantec.cr>
2022-10-28 01:48:58 -0600

    Creacion de los .cpp para las pruebas unitarias

--- convert_to_binary_check.cpp
+++ convert_to_binary_check.cpp
@@ -2,7 +2,7 @@
 
 #include "gtest/gtest.h"
 
-int main() {
+int convert_test() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ read_binary_check.cpp
@@ -0,0 +1,7 @@
+//
+// Created by alisson on 28/10/22.
+//
+
+#include <iostream>
+#include "gtest/gtest.h"
+
